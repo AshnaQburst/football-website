479d205185c81693ea9dcab7a8234acc7087298c
ashnaqburst <ashna@qburst.com>
2018-06-28 18:05:20 +0530

    created ui for gallery page from json file of images

--- script/script.js
+++ script/script.js
@@ -1,25 +1,87 @@
 var data = {
     "items": [
         {
-            "url": "../images/1.jpeg",
+            "url": "images/1.jpeg",
         }, 
         {
-            "url": "../images/2.jpeg",
+            "url": "images/2.jpeg",
         }, 
         {
-            "url": "../images/3.jpeg",
+            "url": "images/3.jpeg",
         }, 
         {
-            "url": "../images/4.jpeg",
+            "url": "images/4.jpeg",
         },
         {
-            "url": "../images/5.jpeg",
+            "url": "images/5.jpeg",
         },
         {
-            "url": "../images/6.jpeg",
+            "url": "images/6.jpeg",
+        },
+        {
+            "url": "images/7.jpeg",
+        },
+        {
+            "url": "images/8.jpeg",
+        },
+        {
+            "url": "images/9.jpeg",
+        },
+        {
+            "url": "images/10.jpeg",
+        },
+        {
+            "url": "images/1.jpeg",
+        },
+        {
+            "url": "images/2.jpeg",
+        },
+        {
+            "url": "images/3.jpeg",
+        },
+        {
+            "url": "images/4.jpeg",
+        },
+        {
+            "url": "images/5.jpeg",
+        },
+        {
+            "url": "images/6.jpeg",
+        },
+        {
+            "url": "images/7.jpeg",
+        },
+        {
+            "url": "images/8.jpeg",
+        },
+        {
+            "url": "images/9.jpeg",
+        },
+        {
+            "url": "images/10.jpeg",
+        },
+        {
+            "url": "images/1.jpeg",
+        },
+        {
+            "url": "images/2.jpeg",
+        },
+        {
+            "url": "images/3.jpeg",
+        },
+        {
+            "url": "images/4.jpeg",
+        },
+        {
+            "url": "images/5.jpeg",
+        },
+        {
+            "url": "images/6.jpeg",
         }
     ]
 };
-$.each(data.items, function (i, f) {
-    $("ul").append("<li><img src='" + f.url +  " </li>");
+$(document).ready(function(){
+    $.each(data.items, function (i, f) {
+        $("ul").append("<li><a href=''><img src='" + f.url +  " '</a></li>");
+    });
 });
