e65f024a9cce062e82a325703d5a25e5fbf1a76e
ashnaqburst <ashna@qburst.com>
2018-07-05 11:36:01 +0530

    updated code as per comments

--- script/script.js
+++ script/script.js
@@ -1,69 +1,71 @@
 
 $(document).ready(function() {
-
-
   $.getJSON( "script/gallery.json", function( data ) {
+  
+    // invoking previous slide function
+  $("#myModal .slideshow-container .prev").click(function(){
+    slideSwitch('prev');
+  });
+  
+  // invoking next slide function
+  $("#myModal .slideshow-container .next").click(function(){
+    slideSwitch('next');
+  });
+
+  //close modal on click of close button
+  $("#myModal .close").click(function(){
+    $("#myModal").removeClass("block");
+    $("#myModal .slideshow-container .slide-images").empty();
+    $("#myModal .related").empty();
+  });
+
+  // exiting from modal while clicking outside the modal
+   window.onclick = function(event) {
+    if (event.target == $('#myModal')[0]) {
+        $("#myModal").removeClass("block");
+        $("#myModal .slideshow-container .slide-images").empty();
+        $("#myModal .related").empty();
+    }
+  };
+
+  // function for switching slide
+  function slideSwitch(slidepos){
+    var active = $('#myModal .my-slides.fade.block');
+    var slides = $('#myModal .my-slides'); 
+    if (slidepos == 'next') {
+      var next = active.data('attr') === "last" ? slides.first() : active.next();
+      next.addClass('block');            
+    }
+    else{
+      var prev = active.data('attr') === "first" ? slides.last() : active.prev(); 
+      prev.addClass('block');
+    }
+    active.removeClass('block');
+  }
+    // appending gallery items
     $.each(data.items, function(i, f) {
-      $("ul").append(
+      $('.gallery-list').append(
       "<li class='gallery-item cp' id='" +(i + 1) +"'><a><img  src='" +f.url +" '</a></li>"
       );
-      });
-      var modal = document.getElementById("myModal");
-      $(".gallery-item").on("click", function() {
+    });   
+
+    // appending related items on the click of a gallery item
+    $(".gallery-item").on("click", function() {
         $(".modal").addClass("block");
         var pos = $(this).attr("id");
         var index = data.items[pos - 1];
-        function slideSwitch(slidepos){
-          var $active = $('.my-slides.fade.block');
-          var $slides = $('.my-slides'); 
-          if (slidepos == 'next') {
-            var $next = $active.data('attr') === "last" ? $slides.first() : $active.next();
-            $next.addClass('block');            
-          }
-          else{
-            var $prev = $active.data('attr') === "first" ? $slides.last() : $active.prev(); 
-            $prev.addClass('block');
-          }
-          $active.removeClass('block');
-        }
         // appending the modal content
-        $(".modal-content").append(
-          "<i class='fa fa-close close cp'></i>"+
-          "<div class='col-50 main'>"+
-          " <div class='slideshow-container'>"+
+        $("#myModal .slideshow-container .slide-images").append(
           " <img class='my-slides fade block' data-attr='first' src='" +index.related[0].url1 +"' style='width:100%'>" +
           " <img class='my-slides fade ' src='" +index.related[0].url2 +"' style='width:100%'>" +
-          " <img class='my-slides fade ' data-attr='last' src='" +index.related[0].url3 +"' style='width:100%'>"+
-          " <a class='prev'> &#10094; </a>"+
-          " <a class='next'>&#10095;</a>"+
-          "</div></div>" +
-          "<div class='col-50 related'>"+
+          " <img class='my-slides fade ' data-attr='last' src='" +index.related[0].url3 +"' style='width:100%'>"
+        );
+        $("#myModal .related").append(
           "<img src='" +index.related[0].url2 +"'>"+
           "<img src='" +index.related[0].url3 +"'>"+
-          "<img src='" +index.related[0].url4 +"'>"+
-          "</div>"
-        );
-        // invoking previous slide function
-        $(".prev").click(function(){
-          slideSwitch('prev');
-        });
-        // invoking next slide function
-        $(".next").click(function(){
-          slideSwitch('next');
-        });
-        $(".close").click(function(){
-          $(".modal").removeClass("block");
-          $(".modal-content").empty();
-        });
-    
+          "<img src='" +index.related[0].url4 +"'>"
+        );    
       });
-      // exiting from modal while clicking outside the modal
-      window.onclick = function(event) {
-        if (event.target == modal) {
-          $(".modal").removeClass("block");
-          $(".modal-content").empty();
-        }
-      };
   });
 });
 
